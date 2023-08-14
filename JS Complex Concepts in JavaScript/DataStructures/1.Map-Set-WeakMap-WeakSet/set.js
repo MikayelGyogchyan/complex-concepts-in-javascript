0a746ea1c4966e6cfa834c24e5401025ca44f264
Mikayel <gyogchyanmikayel@gmail.com>
2023-08-14 14:27:16 +0000

    Add files via upload

---
+++ JS Complex Concepts in JavaScript/DataStructures/1.Map-Set-WeakMap-WeakSet/set.js
@@ -0,0 +1,31 @@
+const set = new Set([1, 2, 3, 3, 3, 4, 5, 5, 6])
+
+set.add(10).add(20).add(30).add(20)
+
+
+// console.log(set)
+// console.log(set.has(30))
+// console.log(set.size)
+// console.log(set.delete(1))
+// console.log(set.size)
+// console.log(set.clear())
+// console.log(set.size)
+
+// console.log(set.values());
+// console.log(set.keys());
+// console.log(set.entries());
+
+// for(let key of set) {
+//   console.log(key); 
+// }
+
+function uniqueValues(array) {
+  return [...new Set(array)] 
+  return Array.from(new Set(array)) 
+}
+
+console.log(uniqueValues([1, 2, 2, 4, 4, 4, 5, 5, 6, 6, 6]));
+
+
+
+
