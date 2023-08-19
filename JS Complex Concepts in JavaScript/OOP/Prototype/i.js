4f16a47d57e8d02bc31394e057377b6fb00f05a5
Mikayel <gyogchyanmikayel@gmail.com>
2023-08-19 21:25:26 +0000

    Add files via upload

---
+++ JS Complex Concepts in JavaScript/OOP/Prototype/i.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// const person = {
+//   name: 'Maxim',
+//   age: 25,
+//   greet() {
+//     console.log('Greet !');
+//   }
+// }
+
+// person.greet()   
+// person.sayHi()   
+// person.toString() 
+
+//
+
+// const person2 = new Object ({
+//   name: 'Maxim',
+//   age: 25,
+//   greet() {
+//     console.log('Greet !');
+//   }
+// })
+
+// Object.prototype.sayHello = () => {
+//   console.log('Hello');
+// }
+
+
+// person2.sayHello() 
+
+// const joe = Object.create(person2)
+
+// joe.greet() 
+// joe.toString()
+
+
+// const str = 'I am String'
+
+// const str2 = new String('I am String')
+// str.sayHello() 
