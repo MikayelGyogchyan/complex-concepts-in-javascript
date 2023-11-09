49282ee1a388744d70f0db3f33458047211769d4
Mikayel <gyogchyanmikayel@gmail.com>
2023-11-09 19:26:03 -0100

    Add files via upload
    Factory

---
+++ JS Complex Concepts in JavaScript/JavaScript Patterns. Design Patterns/1 creational/2_factory.js
@@ -0,0 +1,56 @@
+// Factory 
+
+class SimpleMembership {
+  constructor(name) {
+    this.name = name
+    this.cost = 50
+  }
+}
+
+class StandardMembership {
+  constructor(name) {
+    this.name = name
+    this.cost = 150
+  }
+}
+
+class PremiumMembership {
+  constructor(name) {
+    this.name = name
+    this.cost = 500
+  }
+}
+
+class MemberFactory {
+  static list = {
+    simple: SimpleMembership,
+    standard: StandardMembership,
+    premium: PremiumMembership
+  }
+
+  create(name, type = 'simple') {
+    const Membership = MemberFactory.list[type] || MemberFactory.list.simple
+    const member = new Membership(name)
+    member.type = type
+    member.define = function() {
+      console.log(`${this.name} (${this.type}): ${this.cost}`)
+    }
+    return member
+  }
+}
+
+const factory = new MemberFactory()
+
+const members = [
+  factory.create('Mikayel', 'simple'),
+  factory.create('Joe', 'premium'),
+  factory.create('John', 'standard'),
+  factory.create('Jack', 'premium'),
+  factory.create('Jimi')
+]
+
+// console.log(members);
+
+members.forEach(m => {
+  m.define()
+})
